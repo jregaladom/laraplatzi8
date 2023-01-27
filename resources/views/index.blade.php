923406b490e0f3bccc84b78fc2ad62129e505466
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-01-27 13:00:39 -0600

    Avance curso

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos Cursos</h1>
+    <h2 class="text-xl text-gray-600">Formate online como profesional de tecnología</h2>
+    <h3 class="text-lg text-gray-600">Gana bien programando</h3>
+</div>
+@endsection
